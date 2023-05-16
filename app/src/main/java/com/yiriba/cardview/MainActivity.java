52c362f3b6f906e39c4d8d66c72d3ea19636f107
Ibrahim Diabate <ikdiabate9@gmail.com>
2023-05-15 21:24:40 -0700

    Remove previous code

--- app/src/main/java/com/yiriba/cardview/MainActivity.java
+++ app/src/main/java/com/yiriba/cardview/MainActivity.java
@@ -1,31 +1,15 @@
 package com.yiriba.cardview;
 
-import androidx.fragment.app.FragmentActivity;
-import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.google.android.material.tabs.TabItem;
-import com.google.android.material.tabs.TabLayout;
-import com.yiriba.cardview.adapter.MyPagerAdapter;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        TabLayout tabLayout = findViewById(R.id.tabLayout);
-        TabItem tabChat = findViewById(R.id.ti_chat);
-        TabItem tabStatus = findViewById(R.id.ti_status);
-        TabItem tabCalls = findViewById(R.id.ti_calls);
-
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-
-        // Join the TabLayout with the ViewPager
-        tabLayout.setupWithViewPager(viewPager);
 
     }
 
