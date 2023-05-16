7de54e666263da6e0c346503cefe5fa877bc0c3c
Ibrahim Diabate <ikdiabate9@gmail.com>
2023-05-15 22:24:32 -0700

    Add bottom navigation bar

--- app/src/main/java/com/yiriba/cardview/MainActivity.java
+++ app/src/main/java/com/yiriba/cardview/MainActivity.java
@@ -1,16 +1,44 @@
 package com.yiriba.cardview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.yiriba.cardview.fragment.FirstFragment;
+import com.yiriba.cardview.fragment.SecondFragment;
+import com.yiriba.cardview.fragment.ThirdFragment;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        // Default fragment at launch
+        bottomNavigationView.setSelectedItemId(R.id.home);
 
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        FirstFragment first = new FirstFragment();
+        SecondFragment second = new SecondFragment();
+        ThirdFragment third = new ThirdFragment();
+
+        if (item.getItemId() == R.id.home) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, second).commit();
+        } else if (item.getItemId() == R.id.profile) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, first).commit();
+        } else if (item.getItemId() == R.id.settings) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, third).commit();
+        }
+        return true;
+    }
 }
